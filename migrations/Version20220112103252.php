70e39d361d9f2541a09186351f82919d708cb599
florian <martinflorian2@gmail.com>
2022-01-12 14:06:36 +0100

    staff entity

--- migrations/Version20220112103252.php
+++ migrations/Version20220112103252.php
@@ -20,7 +20,31 @@ final class Version20220112103252 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, mail VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role JSON DEFAULT NULL, nickname VARCHAR(255) DEFAULT NULL, phone VARCHAR(12) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) DEFAULT NULL, birthdate DATETIME DEFAULT NULL, license TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` 
+        (
+            id INT AUTO_INCREMENT NOT NULL, 
+            mail VARCHAR(255) NOT NULL, 
+            password VARCHAR(255) NOT NULL, role JSON DEFAULT NULL, 
+            nickname VARCHAR(255) DEFAULT NULL, 
+            phone VARCHAR(12) DEFAULT NULL, 
+            last_name VARCHAR(255) DEFAULT NULL, 
+            first_name VARCHAR(255) DEFAULT NULL, 
+            birthdate DATETIME DEFAULT NULL, 
+            license TINYINT(1) NOT NULL, 
+            PRIMARY KEY(id),
+            
+        ),
+        -- CREATE TABLE staff (
+        --         id INT AUTO_INCREMENT NOT NULL, 
+        --         last_name VARCHAR(255) NOT NULL, 
+        --         first_name VARCHAR(255) NOT NULL, 
+        --         position VARCHAR(255) NOT NULL, 
+        --         phone VARCHAR(12) DEFAULT NULL, 
+        --         email VARCHAR(255) DEFAULT NULL, 
+        --         PRIMARY KEY(id)
+        --     )
+                DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+            );
     }
 
     public function down(Schema $schema): void
---
+++ src/Entity/Staff.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StaffRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StaffRepository::class)
+ */
+class Staff
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $position;
+
+    /**
+     * @ORM\Column(type="string", length=12, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getPosition(): ?string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
