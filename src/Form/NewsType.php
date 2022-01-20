23c33d00c61bb1373302b73254af09d2b0de29b2
Alexis Boulangé <alexisboulange88@gmail.com>
2022-01-20 16:26:01 +0100

    CRUD entities

---
+++ src/Form/NewsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\News;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('picture')
+            ->add('slug')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('user')
+            ->add('team')
+            ->add('event')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => News::class,
+        ]);
+    }
+}
---
+++ src/Form/PlayerType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('number')
+            ->add('birthdate')
+            ->add('picture')
+            ->add('slug')
+            ->add('relation')
+            ->add('position')
+            ->add('stats')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+        ]);
+    }
+}
