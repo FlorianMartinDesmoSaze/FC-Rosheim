a1920ecdde4ff5ba76bb484d72b659e6873da99c
Michel GRY <alss-cdatp21-gmi@ccicampus.fr>
2022-01-25 15:28:34 +0000

    page event

---
+++ Event.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Event
+{
+    public const EVT_SPORT = 'sportif';
+    public const EVT_ASSO = 'associatif';
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist(): void
+    {
+        $this->slug = strtolower((new AsciiSlugger())->slug($this->title));
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate(): void
+    {
+        $this->slug = strtolower((new AsciiSlugger())->slug($this->title));
+    }
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $endDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity=MultiPicture::class, mappedBy="event")
+     */
+    private $multiPictures;
+
+    /**
+     * @ORM\OneToOne(targetEntity=News::class, mappedBy="event", cascade={"persist", "remove"})
+     */
+    private $news;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="events")
+     */
+    private $user;
+
+    public function __construct()
+    {
+        $this->multiPictures = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|MultiPicture[]
+     */
+    public function getMultiPictures(): Collection
+    {
+        return $this->multiPictures;
+    }
+
+    public function addMultiPicture(MultiPicture $multiPicture): self
+    {
+        if (!$this->multiPictures->contains($multiPicture)) {
+            $this->multiPictures[] = $multiPicture;
+            $multiPicture->setEvent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMultiPicture(MultiPicture $multiPicture): self
+    {
+        if ($this->multiPictures->removeElement($multiPicture)) {
+            // set the owning side to null (unless already changed)
+            if ($multiPicture->getEvent() === $this) {
+                $multiPicture->setEvent(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getNews(): ?News
+    {
+        return $this->news;
+    }
+
+    public function setNews(?News $news): self
+    {
+        // unset the owning side of the relation if necessary
+        if ($news === null && $this->news !== null) {
+            $this->news->setEvent(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($news !== null && $news->getEvent() !== $this) {
+            $news->setEvent($this);
+        }
+
+        $this->news = $news;
+
+        return $this;
+    }
+}
