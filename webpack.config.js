7ae2d96d9019e52941ee576aaffc178d21054048
vicky <rigaut.theotime@gmail.com>
2022-02-26 12:06:30 +0100

    added leave warning for admin panel, renamed js

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('adminUsersEdit', './assets/js/adminPanelUsersEdit')
+    .addEntry('adminSubmitWarning', './assets/js/adminSubmitWarning')
+    .addEntry('adminLeaveWarning', './assets/js/adminLeaveWarning')
     .addEntry('news', './assets/js/news.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
