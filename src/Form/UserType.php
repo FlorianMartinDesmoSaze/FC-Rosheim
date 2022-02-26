fb92c1b1162df6c2a04e68efbd4d6ff8883953ab
vicky <rigaut.theotime@gmail.com>
2022-02-26 12:10:25 +0100

    changed birthdate DateType widget

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -27,14 +28,8 @@ class UserType extends AbstractType
             ->add('phone')
             ->add('lastName')
             ->add('first_name')
-            ->add('birthdate', null, [
-                'widget' => 'choice',
-                'years' => range(date('Y'), date('Y') - 100),
-                'placeholder' => [
-                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
-                ],
-                // 'months' => range(date('m'), 12),
-                // 'days' => range(date('d'), 31),
+            ->add('birthdate', DateType::class, [
+                'widget' => 'single_text',
             ])
             ->add('license')
             ->add('isVerified')
