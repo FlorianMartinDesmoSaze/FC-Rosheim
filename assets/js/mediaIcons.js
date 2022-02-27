63771ed5dd864db55f0f05749d8a53d71d87c150
florian <martinflorian2@gmail.com>
2022-02-27 22:40:26 +0100

    slightly improved social media icons animation

--- assets/js/mediaIcons.js
+++ assets/js/mediaIcons.js
@@ -1,10 +1,14 @@
-const trigger = document.querySelector('.fa-solid');
-const links = document.querySelectorAll('.fa-brands');
+const trigger = document.querySelector('.fa-up-right-from-square');
 
-trigger.addEventListener('click', ()=> {
-    console.log(links);
-links.forEach(link => {
-    console.log(link);
-    link.classList.toggle('active');
-});
-});
+trigger.addEventListener('click', () => {
+    const facebook = document.querySelector('.fa-facebook-square')
+    console.log(facebook);
+
+    const youtube = document.querySelector('.fa-youtube')
+    console.log(youtube);
+
+    facebook.classList.toggle('active')
+    setTimeout(() => {
+        youtube.classList.toggle('active')
+    }, 50);
+})
