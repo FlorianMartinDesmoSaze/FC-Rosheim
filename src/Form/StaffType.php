326f6796a445d316cff0df049dcca946c0cea5bf
vkyfox <rigaut.theotime@gmail.com>
2022-02-28 11:59:57 +0100

    updated staff ud

--- src/Form/StaffType.php
+++ src/Form/StaffType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Staff;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class StaffType extends AbstractType
 {
@@ -17,7 +19,24 @@ class StaffType extends AbstractType
             ->add('position')
             ->add('phone')
             ->add('email')
-            ->add('picture')
+            ->add('picture', FileType::class, [
+                'label' => 'Photo du staff',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'required' => false,
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1000k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                    ])
+                ],
+            ])
             ->add('user')
         ;
     }
