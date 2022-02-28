ca7127178243b2c0936babc717be72f1a3210e4a
vkyfox <rigaut.theotime@gmail.com>
2022-02-28 11:58:15 +0100

    changed message: editing is reversible provided you remember what it was before

--- assets/js/adminSubmitWarning.js
+++ assets/js/adminSubmitWarning.js
@@ -2,7 +2,7 @@ const adminPanelSaveButtonSelector = document.querySelector('#admin_save_button'
 const adminPanelDeleteButtonSelector = document.querySelector('#admin_delete_button');
 
 adminPanelSaveButtonSelector.addEventListener('click', (e) => {
-    if (confirm("Êtes-vous sûr·e de vouloir modifier ces données ? Ces changements sont irréversibles.") == true) {
+    if (confirm("Êtes-vous sûr·e de vouloir modifier ces données ?") == true) {
         console.log("form submitted")
     } else {
         e.preventDefault()
