2c74f24e9fb30642ee9b8a0b589562f71d035d0b
Alexis Boulangé <alexisboulange88@gmail.com>
2022-02-28 20:57:12 +0100

    images

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use App\Entity\News;
 use App\Form\NewsType;
+use App\Service\FileUploader;
 use App\Repository\NewsRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 // use App\Entity\Team;
 
 /**
@@ -33,13 +36,20 @@ class NewsController extends AbstractController
      * @Route("/new", name="news_new", methods={"GET", "POST"})
      * @IsGranted("ROLE_ADMIN")
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $news = new News();
         $form = $this->createForm(NewsType::class, $news);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $pictureNews = $form->get('picture')->getData();
+            // this condition is needed because the picture's field is not required
+            // so the file must be processed only when a file is uploaded
+            if ($pictureNews) {
+                $pictureFileName = $fileUploader->upload($pictureNews);
+                $news->setPicture($pictureFileName);
+            }
             $entityManager->persist($news);
             $entityManager->flush();
 
@@ -73,19 +83,35 @@ class NewsController extends AbstractController
      * @Route("/{id}/edit", name="news_edit", methods={"GET", "POST"})
      * @IsGranted("ROLE_ADMIN")
      */
-    public function edit(Request $request, News $news, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, News $news, NewsRepository $newsRepository,
+        EntityManagerInterface $entityManager, FileUploader $fileUploader, int $id): Response
     {
-        // $team = $news->getTeam();
-
-        // if ($news->getTeam() !== $team) {
-        //     $this->addFlash('warning', "Higher rights required");
-        //     throw $this->createAccessDeniedException;
-        // }
-
         $form = $this->createForm(NewsType::class, $news);
+        $news = $newsRepository->find($id); //we find team by id
+        //we check if an image was already set
+        if($news->getPicture()!=null){
+            //we must transform the image string from DB to File to respect the form types
+            $oldFileName = $news->getPicture(); //we get image name in db
+            $oldFileNamePath = $this->getParameter('images_directory').'/'.$oldFileName; //we get his path
+            $pictureFile = new File($oldFileNamePath); //create file
+            $news->setPicture($pictureFile);
+        }else{ //else we define $oldFileNamePath as null to avoid error when trying to remove an inexistant old file
+            $oldFileNamePath = null;
+        }
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if($news->getPicture()!=null){
+                $filesystem = new Filesystem(); //we use this class in order to use his function remove()
+                $pictureNews = $form->get('picture')->getData();
+                $pictureFileName = $fileUploader->upload($pictureNews);
+                $news->setPicture($pictureFileName);
+                $filesystem->remove($oldFileNamePath); //we delete old picture from application
+            }
+            // else we set old picture
+            else{
+                $news->setPicture($oldFileName);
+            }
             $entityManager->flush();
 
             return $this->redirectToRoute('news_index', [], Response::HTTP_SEE_OTHER);
@@ -104,6 +130,10 @@ class NewsController extends AbstractController
     public function delete(Request $request, News $news, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$news->getId(), $request->request->get('_token'))) {
+            $filesystem = new Filesystem(); //we use this class in order to use his function remove() to remove a file
+            $fileName = $news->getPicture();
+            $fileNamePath = $this->getParameter('images_directory').'/'.$fileName;
+            $filesystem->remove($fileNamePath); //we delete old picture from application
             $entityManager->remove($news);
             $entityManager->flush();
         }
--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -3,24 +3,62 @@
 namespace App\Form;
 
 use App\Entity\News;
+use App\Entity\Team;
+use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class NewsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('picture')
-            ->add('slug')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('user')
-            ->add('team')
-            ->add('event')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('picture', FileType::class, [
+                'label' => 'Photo illustrative',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'required' => false,
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1000k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                    ])
+                ],
+            ])
+            ->add('team', EntityType::class, [
+                'label' => 'Équipe',
+                'class' => Team::class,
+                'choice_label' => 'teamName',
+            ])
+            ->add('event', EntityType::class, [
+                'label' => 'Évènement',
+                'class' => Event::class,
+                'choice_label' => 'title',
+            ])
         ;
     }
 
