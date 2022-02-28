775a99860360d6cdc1eaedadb715a4009322bb2b
vkyfox <rigaut.theotime@gmail.com>
2022-02-28 16:11:30 +0100

    added user

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,6 +19,7 @@ class EventType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
+            ->add('user')
             //->add('type')
             //->add('type', EntityType::class,['class'=>Event::class,'choice_label'=>'type','expanded' => true,] )
             ->add('type',ChoiceType::class, [
