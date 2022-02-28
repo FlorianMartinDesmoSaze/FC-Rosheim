5c4d2941b4f825f53ec51ec2225f03a3258394d4
florian <martinflorian2@gmail.com>
2022-02-28 20:19:56 +0100

    small fixes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\NewsRepository;
-use Symfony\Config\WhiteOctoberBreadcrumbsConfig;
-// WhiteOctoberBreadcrumbsConfig $breadcrumbs
 use WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs;
 class HomeController extends AbstractController
 {
@@ -16,10 +14,6 @@ class HomeController extends AbstractController
      */
     public function index(NewsRepository $newsRepository, Breadcrumbs $breadcrumbs): Response
     {
-
-        // $breadcrumbs = $this->get("white_october_breadcrumbs");
-    
-        // Simple example
         $breadcrumbs->addItem("Home", $this->generateUrl("home"));
 
         $news = $newsRepository->findLastThreeNews();
